ea097090b5262a22e5b1e6df31ef3cf8e24217d8
Mohamed Errazki <errazkim@ensipc353.ensimag.fr>
2022-04-01 12:09:33 +0200

    .

--- tp2/fct_affine.s
+++ tp2/fct_affine.s
@@ -12,24 +12,26 @@ uint32_t affine(uint32_t a, uint32_t b, uint32_t x)
 fonction :
      affine  : non feuille
 contexte :
-  	a : registre a0; pile *(sp+0)
-  	b : registre a1; pile *(sp+4)
-  	x : registre a2; pile *(sp+8)
-  	ra : pile *(sp+12)
+     a      : registre a0; pile *(sp+0)
+     b      : registre a1; pile *(sp+4)
+     x      : registre a2; pile *(sp+8)
+     ra               : pile *(sp+12)
  */
 affine:
-addi sp, sp , -4*4
-sw a0, 0(sp)
-sw a1, 4(sp)
-sw a2, 8(sp)
-sw ra, 12(sp)
+    addi sp, sp, -16
+    sw ra, 12(sp)
+    sw a0, 0(sp)
+    sw a1, 4(sp)
+    sw a2, 8(sp)
+
+/* A compléter */
 affine_fin_prologue:
-lw a0 , 8(sp)
-lw a1 , 0(sp)
-jal mult
-lw a1, 4(sp)
-add a0 , a0 , a1
+    lw a0, 8(sp)
+    lw a1, 0(sp)
+    jal mult
+    lw a1, 4(sp)
+    add a0, a0, a1
 affine_debut_epilogue:
-	lw ra, 12(sp)
-	addi sp,sp,16
-    	ret
+    lw ra, 12(sp)
+    addi sp, sp, 16
+    ret
--- tp2/fct_fact.s
+++ tp2/fct_fact.s
@@ -16,23 +16,30 @@ uint32_t fact(uint32_t n)
 fonction :
      fact  : non feuille
 contexte :
-     ra         : pile *(sp+4)
-     n          : registre a0 ; pile *(sp+0)
+     n      : registre a0; pile *(sp+0)
+     ra               : pile *(sp+4)
  */
 fact:
-addi sp,sp , -8
-sw a0 , 0(sp)
-sw ra , 4(sp)
+    addi sp, sp, -8
+    sw ra, 4(sp)
+    sw a0, 0(sp)
+
+/* A compléter */
 fact_fin_prologue:
-li t1, 1
-bgt a0, t1 , else
-mv a0 , t1
-j fact_debut_epilogue
-else : addi a0, a0 , -1
-jal fact
-lw t0 , 0(sp)
-mul a0, a0 , t0
+if:
+    li t0, 1
+    slti t0, a0, 2
+    beqz t0, else
+    mv a0, t0
+    j fact_debut_epilogue
+else:
+    addi a0, a0, -1
+    jal fact
+    lw t1, 0(sp)
+    mul a0, a0, t1
+    j fact_debut_epilogue
+
 fact_debut_epilogue:
-lw ra, 4(sp)
-addi sp, sp , 8
+    lw ra, 4(sp)
+    addi sp, sp, 8
     ret
